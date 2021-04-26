f5c49bd87efdb5a386f91ae8f43b887f3d25521d
Daniel Vernon <danpvernon@gmail.com>
2021-04-26 19:53:21 +0100

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,15 @@ module.exports = {
         icon: `content/assets/shator-icon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-178683449-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
   ],
